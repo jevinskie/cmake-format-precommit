676e3fed3b5fdcf23981abf36d48abfaf7e0480a
Jevin Sweval <jevinsweval@gmail.com>
2023-10-30 18:41:05 -0400

    point to jevinskie/cmake_format v0.6.13.jev1

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name="cmakelang-precommit-dummy",
     version="0.0.0",
-    install_requires=["cmakelang==0.6.13"]
+    install_requires=["cmakelang @ git+https://github.com/jevinskie/cmake_format@v0.6.13.jev1#egg=cmakelang"]
 )
